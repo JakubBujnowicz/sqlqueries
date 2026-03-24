6d48fd2e3fbbf0725fc38974751263536da3c2cb
JakubBujnowicz <bujnowiczgithub@gmail.com>
2026-03-24 17:51:29 +0100

    feature: Completed first version of sql_select()

--- R/sql_select.R
+++ R/sql_select.R
@@ -1,32 +1,72 @@
-sql_select <- function(..., .distinct = FALSE, .top_n = NULL, .defuse = TRUE)
+#' Construct an SQL SELECT statement
+#'
+#' @param ... Columns to select. Can be unquoted column names, character strings,
+#'   or results of other sql queries (e.g. [sql_case()]).
+#' @param .distinct a logical value. If `TRUE`, adds "DISTINCT" to the query.
+#' @param .top a single number. If provided, adds a "TOP" clause.
+#' @param .top_percent a logical value. If `TRUE`, adds "PERCENT" to the "TOP"
+#'   clause.
+#' @param .top_with_ties a logical value. If `TRUE`, adds "WITH TIES" to the
+#'   "TOP" clause.
+#' @param .defuse Logical. Whether to defuse the input arguments.
+#'
+#' @return A character string representing the SQL SELECT statement, with S3
+#'   class 'sql_select'.
+#' @export
+#'
+sql_select <- function(..., .distinct = FALSE,
+                       .top = NULL, .top_percent = FALSE, .top_with_ties = FALSE,
+                       .defuse = TRUE)
 {
-    assert_flag(.distinct)
-    assert_flag(.defuse)
-    assert_count(.top_n, positive = TRUE, null.ok = TRUE)
+    checkmate::assert_flag(.distinct)
+    checkmate::assert_count(.top, null.ok = TRUE)
+    checkmate::assert_flag(.top_percent)
+    checkmate::assert_flag(.top_with_ties)
+    checkmate::assert_flag(.defuse)
 
     cols <- .columns_picker(..., .defuse = .defuse)
 
     rslt <- .new_sql(class = "sql_select",
                      fields = list(columns = cols,
                                    distinct = .distinct,
-                                   top_n = .top_n))
+                                   top = .top,
+                                   top_percent = .top_percent,
+                                   top_with_ties = .top_with_ties))
     rslt <- .sql_parse(rslt)
     return(rslt)
 }
 
 
-
+#' Internal parser for `sql_select` objects
+#'
+#' @param x A sql_select object.
+#' @param fields Fields of the sql_select object.
+#' @param ... additional arguments.
+#'
+#' @return A character string with parsed string.
+#' @keywords internal
+#'
 .parse.sql_select <- function(x, fields, ...)
 {
     header <- "SELECT"
     if (fields$distinct) {
         header <- paste(header, "DISTINCT")
     }
-    if (!is.null(fields$top_n)) {
-        header <- paste0(header, " TOP ", fields$top_n)
+
+    if (!is.null(fields$top)) {
+        header <- sprintf("%s TOP (%i)", header, fields$top)
+
+        if (fields$top_percent) {
+            header <- paste0(header, " PERCENT")
+        }
+        if (fields$top_with_ties) {
+            header <- paste0(header, " WITH TIES")
+        }
     }
 
     rslt <- paste(header, .columns_parser(fields$columns), sep = "\n")
     rslt <- .indent(rslt, by = 4)
     return(rslt)
 }
+
+
---
+++ tests/testthat/test-sql_select.R
@@ -0,0 +1,94 @@
+library(data.table)
+
+# Functions
+.sqldf <- function(x) as.data.table(sqldf::sqldf(x))
+
+# Test data
+dt <- data.table(col1 = 1:3, col2 = c(4, 4, 5), col3 = c(5, 5, 5))
+
+
+# Tests ------------------------------------------------------------------------
+test_that("sql_select() handles basic column selection", {
+    query <- sql(select("col1", "col3"), from("dt"))
+    target <- dt[, list(col1, col3)]
+
+    expect_equal(.sqldf(query), target)
+})
+
+test_that("sql_select() handles distinct", {
+    query <- sql(select("col3", .distinct = TRUE), from("dt"))
+    target <- unique(dt[, list(col3)])
+
+    expect_equal(.sqldf(query), target)
+
+    query <- sql(select(vars(col1, col3), .distinct = TRUE), from("dt"))
+    target <- unique(dt[, list(col1, col3)])
+
+    expect_equal(.sqldf(query), target)
+})
+
+# sqldf does not handle SQLServer "TOP" syntax, can't test against data as
+# easily
+## TODO: Figure out a way to test it
+test_that("sql_select() handles TOP: text based", {
+    query <- sql_select("*", .top = 2)
+    expect_match(query, "SELECT TOP \\(2\\)")
+
+    query <- sql_select("*", .top = 2, .top_percent = TRUE)
+    expect_match(query, "SELECT TOP \\(2\\) PERCENT")
+
+    query <- sql_select("*", .top = 2, .top_with_ties = TRUE)
+    expect_match(query, "SELECT TOP \\(2\\) WITH TIES")
+})
+
+test_that("sql_select() handles aliased columns", {
+    query <- sql(select(NewCol = "col1"), from("dt"))
+    target <- dt[, list(NewCol = col1)]
+
+    expect_equal(.sqldf(query), target)
+})
+
+dt2 <- data.table(a = 1,
+                  able = c(0, 0:2),
+                  about = 3,
+                  asdasd = 4,
+                  glued_var = 5,
+                  Variable = 7,
+                  SecondVar = 8)
+test_that("sql_select() handles multiple selection methods", {
+    target <- dt2[, .(a, able, about,
+                      A = asdasd,
+                      glued = glued_var,
+                      CaseVar = data.table::fcase(able == 1, 1,
+                                                  able == 0, 2,
+                                                  default = 100),
+                      Variable,
+                      SV = SecondVar)]
+    target <- unique(target)
+
+    cs <- sql_case("able IS 1", 1,
+                   "able IS 0", 2,
+                   .else = 100)
+    query <- sql(select(stringr::words[1:3],
+                        A = "asdasd",
+                        glued = "{variable}",
+                        CaseVar = cs,
+                        vars(Variable, SV = SecondVar),
+                        .distinct = TRUE),
+                 from("dt2"),
+                 .glue = list(variable = "glued_var"))
+
+    expect_equal(.sqldf(query), target)
+})
+
+test_that("sql_select() validates inputs", {
+    expect_error(sql_select())
+    expect_error(sql_select("col1", .distinct = "TRUE"))
+    expect_error(sql_select("col1", .top = -1))
+    expect_error(sql_select("col1", .top = "5"))
+    expect_error(sql_select("col1", .top_percent = c(FALSE, FALSE)))
+    expect_error(sql_select("col1", .top_with_ties = c(FALSE, FALSE)))
+    expect_error(sql_select("col1", .defuse = c(FALSE, FALSE)))
+})
+
+
